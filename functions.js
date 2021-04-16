3b6f37f1b39e47d91902558caaa941556ddab8cc
evan-woodworth <evan.woodworth@gmail.com>
2021-04-16 08:57:53 -0700

    complete: interval timeout excercise

--- functions.js
+++ functions.js
@@ -18,56 +18,56 @@
 
 // vowels.forEach(theFunction);
 
-function checkInstagram(username) {
-    // write code and test
-    console.log(`Did ${username} post anything new?`);
-}
-function makePhoneCall(contact) {
-    // write code and test
-    console.log(`Calling ${contact}...`);
-}
-function playSpotify(song) {
-    // write code and test 
-    console.log(`Playing ${song}...`);
-}
-function checkEmail(emailProvider) {
-    // write code and test
-    console.log(`Checking emails from ${emailProvider}...`);
-}
-function sendEmail(message) {
-    // write code and test
-    console.log(`Sending ${message}...`);
-}
-function sendTextMessage(contact) {
-    console.log(`Yo, what's the word ${contact}`);
-}
-function takePicture(filter) {
-    console.log(`Take picture with ${filter} filter`);
-}
-function faceTime(friend) {
-    console.log(`Facetiming my friend ${friend}...`);
-}
-function useMobileDevice(typeOfPhone, funcName, param) {
-    let nameOfFunction = funcName.name;
-    if (typeOfPhone === 'Android' && nameOfFunction === 'faceTime') {
-        console.log('Facetime is only a feature on iPhones');
-        return;
-    }
-    console.log(`Use my ${typeOfPhone} to ${nameOfFunction}`);
-    funcName(param);
-}
-useMobileDevice('iPhone', sendTextMessage, 'Sebastian');
-console.log('---');
-useMobileDevice('Android', faceTime, 'Jake');
-console.log('---');
-useMobileDevice('iPhone', takePicture, 'portrait');
-console.log('---');
-useMobileDevice('iPhone', checkInstagram, 'Bob');
-console.log('---');
-useMobileDevice('iPhone', playSpotify, 'a song');
-console.log('---');
-useMobileDevice('iPhone', checkEmail, 'google');
-console.log('---');
+// function checkInstagram(username) {
+//     // write code and test
+//     console.log(`Did ${username} post anything new?`);
+// }
+// function makePhoneCall(contact) {
+//     // write code and test
+//     console.log(`Calling ${contact}...`);
+// }
+// function playSpotify(song) {
+//     // write code and test 
+//     console.log(`Playing ${song}...`);
+// }
+// function checkEmail(emailProvider) {
+//     // write code and test
+//     console.log(`Checking emails from ${emailProvider}...`);
+// }
+// function sendEmail(message) {
+//     // write code and test
+//     console.log(`Sending ${message}...`);
+// }
+// function sendTextMessage(contact) {
+//     console.log(`Yo, what's the word ${contact}`);
+// }
+// function takePicture(filter) {
+//     console.log(`Take picture with ${filter} filter`);
+// }
+// function faceTime(friend) {
+//     console.log(`Facetiming my friend ${friend}...`);
+// }
+// function useMobileDevice(typeOfPhone, funcName, param) {
+//     let nameOfFunction = funcName.name;
+//     if (typeOfPhone === 'Android' && nameOfFunction === 'faceTime') {
+//         console.log('Facetime is only a feature on iPhones');
+//         return;
+//     }
+//     console.log(`Use my ${typeOfPhone} to ${nameOfFunction}`);
+//     funcName(param);
+// }
+// useMobileDevice('iPhone', sendTextMessage, 'Sebastian');
+// console.log('---');
+// useMobileDevice('Android', faceTime, 'Jake');
+// console.log('---');
+// useMobileDevice('iPhone', takePicture, 'portrait');
+// console.log('---');
+// useMobileDevice('iPhone', checkInstagram, 'Bob');
+// console.log('---');
+// useMobileDevice('iPhone', playSpotify, 'a song');
+// console.log('---');
+// useMobileDevice('iPhone', checkEmail, 'google');
+// console.log('---');
 
 
 
@@ -82,20 +82,29 @@ console.log('---');
 //     return theFinalDish;
 // }
 
-// //excercise 1
-// let count = 10;
-// const countDown = setInterval(
-//     function () {
-//         if (count){
-//             console.log(count);
-//             count--;
-//         } else {
-//             console.log("Blast off!");
-//             clearInterval(countDown);
-//         }
-//     }, 1000
-// );
+setTimeout(
+    function () {
+        console.log("What even is 2000 milliseconds?");
+    }, 2000
+);
+
+//excercise 1
+let count = 10;
+const countDown = setInterval(
+    function () {
+        if (count){
+            console.log(count);
+            count--;
+        } else {
+            console.log("Blast off!");
+            clearInterval(countDown);
+        }
+    }, 1000
+);
 
 // //excercise 2
-
-// let theTimeout = 3200;
+// function annoy() {
+//     console.log('Are we there yet?');
+//     setTimeout(annoy, 1000)
+// }
+// const kids = setTimeout(annoy, 1000);
